4418af5224ce13b0c10155cad5f232b05cffc434
Patricia Rocha <patirocha.dev@gmail.com>
2023-11-06 20:46:33 -0300

    tipo veículo enum

--- src/main/java/br/com/patriciarocha/veiculos/model/enums/TipoVeiculoEnum.java
+++ src/main/java/br/com/patriciarocha/veiculos/model/enums/TipoVeiculoEnum.java
@@ -3,4 +3,11 @@ package br.com.patriciarocha.veiculos.model.enums;
 public enum TipoVeiculoEnum {
 
     AUTOMOVEL, CAMIONETA, ONIBUS, CAMINHAO;
+
+    public enum TipoVeiculo {
+        AUTOMOVEL,
+        CAMIONETA,
+        ONIBUS,
+        CAMINHAO;
+    }
 }
